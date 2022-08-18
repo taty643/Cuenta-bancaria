0d6e10bbb36a772a4cd4bc7f9b55915c62bbeaa5
Tatiana <julyhenao643@gmail.com>
2022-08-18 21:30:05 +0200

    original

--- src/main.java
+++ src/main.java
@@ -3,16 +3,17 @@ public class main {
 
         //instanciar la clase
 
-        Cuenta miCuenta = new Cuenta( "prestamo", 5000);
+        cuenta miCuenta = new cuenta("123456", "Ahorro",150000);
         System.out.println("Saldo minimo: " +miCuenta.getSaldoMinimo());
-        miCuenta.consignar(monto:8000);
+        miCuenta.consignar(80000);
         System.out.println("Saldo Actual: "+miCuenta.getSaldoActual());
-        miCuenta.retirar(monto:250000);
+        miCuenta.retirar(250000);
         System.out.println("Saldo Actual: "+miCuenta.getSaldoActual());
         System.out.println(miCuenta.toString());
         System.out.println("----------------------------------------");
 
-        Cuenta pedroCuenta= new Cuenta (numero: "585858", tipo: "prestamo", valorInicial: 5000);
+        cuenta pedroCuenta= new cuenta ("585858","prestamo",5000);
+        System.out.println(pedroCuenta.toString());
 
 
     }
